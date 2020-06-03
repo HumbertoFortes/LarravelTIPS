e85c25f0bcaedd993f2e6cdcb43c2ebd1b33cb3e
Humberto Fortes <34537571+HumbertoFortes@users.noreply.github.com>
2020-06-04 00:02:07 +0100

    Criando tabelas com o Eloquent

---
+++ ep05 - VERSIONANDO BANCO DE DADOS COM MIGRATIONS/database/migrations/2020_06_03_225112_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('author');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('subtitle');
+            $table->text('content');
+            $table->timestamps();
+            
+            $table->foreign('author')->references('id')->on('users')->onDelete('CASCADE'); //Relacionamento
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
