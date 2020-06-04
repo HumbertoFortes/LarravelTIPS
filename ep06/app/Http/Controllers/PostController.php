a5a1e99f8e06177bdc35b861769b6af4b543c3b9
Humberto Fortes <34537571+HumbertoFortes@users.noreply.github.com>
2020-06-04 20:41:24 +0100

    Adicionado Model

--- ep06/app/Http/Controllers/PostController.php
+++ ep06/app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,8 +12,9 @@ class PostController extends Controller
         return view('form');
     }
 
-    public function debug()
+    public function debug(Request $request)
     {
-        # code...
+        $post = new Post();
+        $post->create($request->except('_token'));
     }
 }
